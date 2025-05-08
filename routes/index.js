d657609fb96800fbc97eeb12e340ab6f548c2fd1
2001010438di <2001010438di@cyber-u.ac.jp>
2025-05-09 01:33:35 +0900

    cahnge message

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  /* タイトルの変更 */	
+  /* res.render('index', { title: 'Express' }); */
+  res.render('index', { title: 'Express 2' });
 });
 
 module.exports = router;
